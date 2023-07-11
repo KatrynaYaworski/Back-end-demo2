09d54919a89c5cdb750aa065962b46791bc988ab
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-11 13:38:03 -0500

    put added

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const { getMovies, addMovie, deleteMovies } = require('./controller.js')
+const { getMovies, addMovie, deleteMovies, updateMovie } = require('./controller.js')
 
 app.get('/api/movies', getMovies)
 
 app.post('/api/movies', addMovie)
 
 app.delete('/api/movies/:id', deleteMovies)
+
+app.put('/api/movies/:id', updateMovie)
 
 app.listen(4004, () => {
     console.log('up on 4004')
