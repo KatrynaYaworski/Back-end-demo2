cf1306b41b5ad69b4c642857dd50af9e3db0bb95
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-11 13:13:45 -0500

    Complete

---
+++ server/controller.js
@@ -0,0 +1,34 @@
+const movieDatabase = require('./db.json');
+let currentId = 11
+
+module.exports = {
+    getMovies: (req, res) => {
+        res.status(200).send(movieDatabase)
+    },
+    addMovie: (req, res) => {
+        const { title, rating, imageURL } = req.body
+
+        movieDatabase.push({
+            id: currentId,
+            title: title,
+            rating: rating,
+            imageURL: imageURL
+        })
+
+        currentId++
+
+        res.status(200).send(movieDatabase)
+    },
+    deleteMovies: (req, res) => {
+        const id = +req.params.id
+
+        for (let i = 0; i < movieDatabase.length; i++) {
+            if (movieDatabase[i].id === id) {
+                movieDatabase.splice(i, 1)
+                res.status(200).send(movieDatabase)
+                return
+            }
+        }
+        res.status(400).send('movie not found')
+    },
+}
